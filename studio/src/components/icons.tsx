f9c9aab093608fb64bc97634a28193a3abdde18b
Max <beppe@live.se>
2022-05-20 12:51:39 +1000

    feat: add studio icons

---
+++ studio/src/components/icons.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Emoji from 'a11y-react-emoji';
+
+export const TeamMemberIcon = () => <Emoji symbol="👨‍🎨" label="team member" />;
+export const PageLayoutIcon = () => <Emoji symbol="🧬" label="page layout" />;
+export const HeadingWithImageIcon = () => (
+  <Emoji symbol="📷💀" label="heading with image" />
+);
+export const PageIcon = () => <Emoji symbol="📄" label="page" />;
+export const BlogIcon = () => <Emoji symbol="📝" label="page" />;
+export const NavigationIcon = () => <Emoji symbol="🧭" label="navigation" />;
+export const SettingsIcon = () => <Emoji symbol="⚙️" label="settings" />;
+export const GlobalSiteLayoutIcon = () => (
+  <Emoji symbol="🌎" label="global site layout" />
+);
+export const RedirectIcon = () => <Emoji symbol="🏹" label="redirect" />;
+export const ProjectIcon = () => <Emoji symbol="💼" label="redirect" />;
